0c9670f2302ff17a4474ce8d5c3787e561ea9104
Lemmy Mwaura <lemminton@gmail.com>
2022-05-11 12:43:55 +0300

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import FormComponent from './components/FormComponent';
+import QuoteList from './components/QuoteList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Form</h2>
+      <FormComponent/>
+      <QuoteList quotes={sampleQuotes}/>
+    </>
   );
 }
+
+const sampleQuotes = [
+  {
+    id:1,
+    author:'Mahatma Gandhi',
+    message:'Live as if you were to die tomorrow. Learn as if you were to live forever.'
+  },
+  {
+    id:2,
+    author:'Friedrich Nietzsche',
+    message:'That which does not kill us makes us stronger.'
+  },
+  {
+    id:3,
+    author:'Virginia Satir',
+    message:'We must not allow other people’s limited perceptions to define us.'
+  },
+  {
+    id:4,
+    author:'Theodore Roosevelt',
+    message:'Do what you can, with what you have, where you are.'
+  }
+]
 
 export default App;
