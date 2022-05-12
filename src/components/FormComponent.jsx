8b56100c9f4c80a60950fe3657de699f731ecee5
Lemmy Mwaura <lemminton@gmail.com>
2022-05-12 21:21:40 +0300

    updated form component

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function FormComponent() {
   return (
-    <div>form component</div>
+    <>
+      <form action="" className='quote-form'>
+        <div className="formgroup">
+          <label htmlFor="author" className="form-label">Author</label>
+          <input type="text" placeholder="Author" id='author' className='form-input'/>
+        </div>
+        <div className="formgroup">
+          <label htmlFor="message" className="form-label">Message</label>
+          <input type="text" placeholder="message" id="message"/>
+        </div>
+      </form>
+    </>
   )
 }
