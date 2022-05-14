b4297347ef6e1bc6266c220f43e4fa4bf7ca047e
Lemmy Mwaura <lemminton@gmail.com>
2022-05-14 15:08:19 +0300

    Icons update + style update

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
+import { BiLike, BiDislike} from 'react-icons/bi';
+import { FaTrashAlt } from 'react-icons/fa';
 
 export default function Quote({message, author}) {
+  const [upvote, setUpvote] = useState(1)
+  const [downvote, setDownvote] = useState(0)
+  
   return (
     <>
       <div className="quote">
@@ -10,10 +16,24 @@ export default function Quote({message, author}) {
         <h1 className="author">{author}</h1>
         <div className="footer">
           <div className="like">
-            <button className="btn">Upvote</button>
-            <button className="btn">Downvote</button>
+            <button 
+              className="btn like-btn"
+              onClick={() => { setUpvote(prevUpvote => prevUpvote + 1) }}
+              ><BiLike color='green'/><span>{ upvote }</span> 
+            </button>
+            <button 
+              className="btn like-btn"
+              onClick={() => { setDownvote(prevDownvote => prevDownvote + 1) }}
+              ><BiDislike color='red'/><span>{ downvote }</span> 
+            </button>
           </div>
-          <div className="total">Total Votes: {1}</div>
+          <div 
+            className="total"
+            > <span>Total Votes: {upvote + downvote}</span> 
+              <button className='btn delete-btn'>
+                <FaTrashAlt color='#9391ff' size="1rem"/>
+              </button>
+          </div>
         </div>
       </div>
     </>
